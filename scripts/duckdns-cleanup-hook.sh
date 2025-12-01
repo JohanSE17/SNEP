48fa0aaa643dcb56118ba24763fd69c40722b037
♪ Johan Espitia ♫ <164941438+JohanSE17@users.noreply.github.com>
2025-12-01 09:26:47 -0500

    Change DuckDNS domain and token values
    Updated DOMAIN and TOKEN variables for DuckDNS.

--- scripts/duckdns-cleanup-hook.sh
+++ scripts/duckdns-cleanup-hook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-DOMAIN="svrsej.duckdns.org"
-TOKEN="9b3ac1b0-3aa3-4d73-bc81-fd4c89c2b383"
+# Tu dominio
+DOMAIN="duckdns.org"
+TOKEN="dfpko...sioo..qwpon"
 
 # Limpia el TXT record
 curl -s "https://www.duckdns.org/update?domains=$DOMAIN&token=$TOKEN&txt=&clear=true"
